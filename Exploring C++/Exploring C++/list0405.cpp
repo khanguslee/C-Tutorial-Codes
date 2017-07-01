a8c92be2506984c41d757293444f05825ccf7952
Kuan Hou Lee <angus.lee809@gmail.com>
2017-07-01 23:56:36 +1000

    Added list0405

---
+++ Exploring C++/Exploring C++/list0405.cpp
@@ -0,0 +1,17 @@
+// Book:	Exploring C++
+// Author:	Ray Lischner
+// Date:	01/07/2017
+// User:	Angus Lee
+
+/// Defining an empty string
+
+#include <stdafx.h>
+#include <iostream>
+#include <ostream>
+#include <string>
+
+int main() {
+	std::string empty;
+	std::cout << "|" << empty << "\n";
+	system("pause");
+}
