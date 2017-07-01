2b852ae86cf6415b99a9e8490b78ceed327421d9
Kuan Hou Lee <angus.lee809@gmail.com>
2017-07-01 22:39:38 +1000

    Added 0402. Tested  \"

---
+++ Exploring C++/Exploring C++/list0402.cpp
@@ -0,0 +1,13 @@
+// Book:	Exploring C++
+// Author:	Ray Lischner
+// Date:	01/07/2017
+// User:	Angus Lee
+
+#include <stdafx.h>
+#include <iostream>
+#include <ostream>
+
+int main() {
+	std::cout << "\"\n";
+	system("pause");
+}
