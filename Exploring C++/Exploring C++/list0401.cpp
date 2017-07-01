686d0b5a84ea957bfe80e4f5f97d94754ad3c486
Kuan Hou Lee <angus.lee809@gmail.com>
2017-07-01 22:37:39 +1000

    Added list0401. Tested out \t

---
+++ Exploring C++/Exploring C++/list0401.cpp
@@ -0,0 +1,14 @@
+// Book:	Exploring C++
+// Author:	Ray Lischner
+// Date:	01/07/2017
+// User:	Angus Lee
+
+#include <stdafx.h>
+#include <iostream>
+#include <ostream>
+
+int main() {
+	std::cout << "Shape\tSides\n" << "-----\t-----\n";
+	std::cout << "Square\t" << 4 << '\n' << "Circle\t?\n";
+	system("pause");
+}
