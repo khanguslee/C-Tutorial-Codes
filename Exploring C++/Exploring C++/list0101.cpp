7f60bb16b8fb641d41daedacf9a91710f339817f
Kuan Hou Lee <angus.lee809@gmail.com>
2017-07-01 20:22:10 +1000

    Code from book typed up

---
+++ Exploring C++/Exploring C++/list0101.cpp
@@ -0,0 +1,53 @@
+// Book:	Exploring C++
+// Author:	Ray Lischner
+// Date:	30/06/2017
+// User:	Angus Lee
+
+// Sort the standard input alphabetically
+// Read lines of text, sort them, and print the results to the standard output.
+// If the command line names a file, read from that file. Otherwise, read from
+// the standard input. The entire input is stored in memory, so don't try
+// this with input files that exceed available RAM.
+
+#include "stdafx.h"
+
+#include <algorithm>
+#include <fstream>
+#include <iostream>
+#include <iterator>
+#include <ostream>
+#include <string>
+#include <vector>
+
+#include <ciso646>	// Added this in because VS needs it for alternative operators
+
+void read(std::istream& in, std::vector<std::string>& text) {
+	std::string line;
+	while (std::getline(in, line))
+		text.push_back(line);
+}
+
+int main(int argc, char* argv[]) {
+	// Part 1
+	// Read the entire input into text. If the command line names a file,
+	// read that file. Otherwise, read the standard input.
+	std::vector<std::string> text;	// Store the lines of text here
+	if (argc < 2)
+		read(std::cin, text);
+	else {
+		std::ifstream in(argv[1]);
+		if (not in) {
+			std::perror(argv[1]);
+			return EXIT_FAILURE;
+		}
+		read(in, text);
+	}
+
+	// Part 2
+	// Sort the text.
+	std::sort(text.begin(), text.end());
+
+	// Part 3
+	// Print the sorted text.
+	std::copy(text.begin(), text.end(), std::ostream_iterator<std::string>(std::cout, "\n"));
+}
