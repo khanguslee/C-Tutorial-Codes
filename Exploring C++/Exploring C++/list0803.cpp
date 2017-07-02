cd83c15bb5d12cfbc7878e67a34616cba6249c8f
Kuan Hou Lee <angus.lee809@gmail.com>
2017-07-02 12:23:29 +1000

    Added list0803

---
+++ Exploring C++/Exploring C++/list0803.cpp
@@ -0,0 +1,30 @@
+// Book:	Exploring C++
+// Author:	Ray Lischner
+// Date:	02/07/2017
+// User:	Angus Lee
+// Using alternative fill characters
+
+#include <stdafx.h>
+#include <iomanip>
+#include <iostream>
+#include <ostream>
+
+int main() {
+	using namespace std;
+	int day(14);
+	int month(3);
+	int year(2006);
+	int dollars(42);
+	int cents(7);
+
+	// Print date in USA order. Later in the book, you will learn how to
+	// handle internationalization.
+
+	cout << "Date: " << setfill('0') << setw(2) << month
+		<< '/' << setw(2) << day
+		<< '/' << setw(2) << year << '\n';
+	cout << "Pay to the order of: CASH\n";
+	cout << "The amount of $" << setfill('*') << setw(8) << dollars << '.'
+		<< setfill('0') << setw(2) << cents << '\n';
+	system("pause");
+}
