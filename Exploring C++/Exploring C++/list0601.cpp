f3f19a3ca169fc2fa0758d86b16f339135f5e532
Kuan Hou Lee <angus.lee809@gmail.com>
2017-07-02 11:25:44 +1000

    Added list0601

---
+++ Exploring C++/Exploring C++/list0601.cpp
@@ -0,0 +1,27 @@
+// Book:	Exploring C++
+// Author:	Ray Lischner
+// Date:	02/07/2017
+// User:	Angus Lee
+// Deliberate Errors
+
+#include <stdafx.h>
+#include <iostream>
+#include <istream>
+#include <ostream>
+
+int main() {
+	std::cout << "This program prints a table of squares.\n" << "Enter the starting value for the table: ";
+	int start(0);
+	std::cin >> start;
+	std::cout << "Enter the ending value for the table: ";
+	int end(start);
+	std::cin >> end;
+	std::cout << "# #^2\n";
+	int x(start);
+	end = end + 1; // exit loop when x reaches end
+	while (x != end) {
+		std::cout << x << " " << x*x << "\n";
+		x = x + 1;
+	}
+	system("pause");
+}
