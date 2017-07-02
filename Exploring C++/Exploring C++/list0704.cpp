1031c561fb219d4ad1bdada7fe29d2121178bf45
Kuan Hou Lee <angus.lee809@gmail.com>
2017-07-02 11:41:16 +1000

    Added list0704

---
+++ Exploring C++/Exploring C++/list0704.cpp
@@ -0,0 +1,18 @@
+// Book:	Exploring C++
+// Author:	Ray Lischner
+// Date:	02/07/2017
+// User:	Angus Lee
+// Compute sum of integers from 10 to 20
+
+#include <stdafx.h>
+#include <iostream>
+#include <ostream>
+
+int main() {
+	int sum(0);
+
+	// Fill in the loop here.
+
+	std::cout << "Sum of 10 to 20 = " << sum << '\n';
+	system("pause");
+}
